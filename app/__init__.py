d122b3ebbc77f4416f9477d654283afd0effab4a
LEGEN DARY MAD <tajay59@gmail.com>
2022-03-02 15:43:47 -0500

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .config import Config
 
 app = Flask(__name__)
@@ -14,5 +15,6 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 # Instantiate Flask-Migrate library here
+migrate = Migrate(app,db)
 
 from app import views
